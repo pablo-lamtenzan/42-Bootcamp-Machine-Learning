ff36aad260099e07a278262a921edab922f1ff9b
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2020-06-06 19:28:29 +0200

    day00 maths done

--- day00/ex08/vec_cost.py
+++ day00/ex08/vec_cost.py
@@ -4,5 +4,5 @@ import numpy as np
 def cost(y, y_hat):
     if y.shape[0] != 1 or y.shape != y_hat.shape:
         return None
-    return sum(((y_hat - y) * (y_hat - y)) / (2 * y.shape[0])) # normally should work
+    return np.sum((np.power(y_hat - y, 2) / (2 * y.shape[0])) # normally should work
 
